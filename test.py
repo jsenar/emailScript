b7465b7c823a55d4f4d80842743dae351ed90e78
John <jsenar@Johns-MacBook-Pro-2.local>
2016-12-07 12:21:59 -0800

    first commit

---
+++ test.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+from getpass import getpass
+
+def main():
+	num_emails = 15
+	start_email = 1
+	#url = "https://acs-webmail.ucsd.edu/squirrelmail/src/right_main.php?use_mailbox_cache=0&startMessage={0}&mailbox=INBOX".format(start_email)
+	driver = webdriver.Firefox()
+	driver.get("https://acs-webmail.ucsd.edu")
+
+	username = raw_input("Please enter your UCSD username: ")
+	pw = getpass("Please enter your password: ")	
+
+	elem = driver.find_element_by_name("login_username")
+	elem.clear()
+	elem.send_keys(username)
+	elem = driver.find_element_by_name("secretkey")
+	elem.clear()
+	elem.send_keys(pw)
+	elem = driver.find_element_by_name("requested_other_imap_server")
+	elem.clear()
+	elem.send_keys("sdcc15.ucsd.edu")
+	elem.send_keys(Keys.RETURN)	
+
+	time.sleep(1)	
+
+	while "ERROR" not in driver.page_source:
+		driver.get("https://acs-webmail.ucsd.edu/squirrelmail/src/right_main.php?use_mailbox_cache=0&startMessage={0}&mailbox=INBOX".format(start_email))
+		driver.find_element_by_link_text("Toggle All").click()
+		driver.find_element_by_name("markRead").click()
+		start_email = start_email + num_emails
+	'''list = driver.find_elements_by_tag_name('a')
+	print list
+	for link in list:
+		print link.get_attribute("href")'''
+	driver.close()
+
+if __name__ == "__main__":
+    main()
+
